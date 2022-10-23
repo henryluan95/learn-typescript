5c84f00097f81b613f83db3387e6cfad15516aeb
Henry Luan <henryluan95@gmail.com>
2022-10-23 16:29:21 -0400

    tupes

--- app.ts
+++ app.ts
@@ -1,7 +1,13 @@
-const person: { name: string; age: number; hobbies: string[] } = {
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string];
+} = {
   name: "Henry",
   age: 30,
   hobbies: ["eat", "sleep", "laugh"],
+  role: [2, "author"],
 };
 
 console.log(person.name);
