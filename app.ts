e197411bf71acdcf153c5127eb70f45f158532a1
Henry Luan <henryluan95@gmail.com>
2022-10-23 16:26:12 -0400

    Array in TS

--- app.ts
+++ app.ts
@@ -1,6 +1,7 @@
-const person: { name: string; age: number } = {
+const person: { name: string; age: number; hobbies: string[] } = {
   name: "Henry",
   age: 30,
+  hobbies: ["eat", "sleep", "laugh"],
 };
 
 console.log(person.name);
