28e3d97235798fddc15f3d7e5f6250b6e40ef218
Henry Luan <henryluan95@gmail.com>
2022-12-02 10:01:20 -0500

    Tuple

--- app.ts
+++ app.ts
@@ -1,6 +1,25 @@
-const person: { name: string; age: number } = {
+// const person: { name: string; age: number } = {
+//   name: "Henry",
+//   age: 30,
+// };
+
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string];
+} = {
   name: "Henry",
   age: 30,
+  hobbies: ["Sports", "Cooking"],
+  role: [2, "author"],
 };
+
+let favoriteActivities: string[];
+favoriteActivities = ["Sports"];
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+}
 
 console.log(person);
